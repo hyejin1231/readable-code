23eee5d40f84b06969a426043eb0c2052578eb9e
hyejin1231 <phj981231@naver.com>
2024-10-15 08:58:51 +0900

    2024.10.15 섹션5. 객체 지향 활용하기 - 숨겨져 있는 도메인 개념 추출하기

--- src/main/java/cleancode/minesweeper/tobe/GameApplication.java
+++ src/main/java/cleancode/minesweeper/tobe/GameApplication.java
@@ -1,12 +1,9 @@
 package cleancode.minesweeper.tobe;
 
-import cleancode.minesweeper.tobe.gamelevel.Advanced;
+import cleancode.minesweeper.tobe.config.GameConfig;
 import cleancode.minesweeper.tobe.gamelevel.Beginner;
-import cleancode.minesweeper.tobe.gamelevel.GameLevel;
 import cleancode.minesweeper.tobe.io.ConsoleInputHandler;
 import cleancode.minesweeper.tobe.io.ConsoleOutputHandler;
-import cleancode.minesweeper.tobe.io.InputHandler;
-import cleancode.minesweeper.tobe.io.OutputHandler;
 
 /**
  * (1) SRP : 단일 책임의 원칙
@@ -16,14 +13,19 @@ import cleancode.minesweeper.tobe.io.OutputHandler;
  * -> 유동적으로 런타임 시점에 원하는 난이도를 선택해서 만들 수 있도록 !!
  *
  * (5) DIP : 의존성 역전 원칙
+ *
+ * 숨겨진 도메인 개념 추출하기
  */
 public class GameApplication {
 
     public static void main(String[] args) {
-        GameLevel gameLevel = new Beginner();
-        InputHandler inputHandler = new ConsoleInputHandler();
-        OutputHandler outputHandler = new ConsoleOutputHandler();
-        MineSweeper mineSweeper = new MineSweeper(gameLevel, inputHandler, outputHandler);
+        GameConfig gameConfig = new GameConfig(
+                new Beginner(),
+                new ConsoleInputHandler(),
+                new ConsoleOutputHandler()
+        );
+
+        MineSweeper mineSweeper = new MineSweeper(gameConfig);
 
         mineSweeper.initialize();
         mineSweeper.run();
